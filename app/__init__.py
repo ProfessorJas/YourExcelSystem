9163dd4346cc12868885b418bc25298dd86beb7f
ProfessorJas <jishenghuang94@hotmail.com>
2019-10-31 16:39:42 +0800

    add login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask     # import Flask object from flask package
+from config import Config
 
 app = Flask(__name__)       # assign the Flask object to the variabel app
+app.config.from_object(Config)
 
 from app import routes      # import routes from app package
