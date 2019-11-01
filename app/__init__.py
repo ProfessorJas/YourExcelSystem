d1cd150c68e540d463c479ccc6fd692b52c29c53
ProfessorJas <jishenghuang94@hotmail.com>
2019-11-01 14:34:16 +0800

    Login finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask     # import Flask object from flask package
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from flask_login import LoginManager
+
 app = Flask(__name__)       # assign the Flask object to the variabel app
 app.config.from_object(Config)
 
-from app import routes      # import routes from app package
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models      # import routes from app package
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql+pymysql://root:123456@localhost/testmydb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
